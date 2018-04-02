19d54e4453a4af413e8b18fb656c213f32789072
Gavin <gavinfielder@gmail.com>
2018-04-02 11:55:58 -0700

    WIP: first number initializer
    Some work towards first numbers initialization.

--- include/Randomizer.h
+++ include/Randomizer.h
@@ -1,10 +1,16 @@
 #ifndef RANDOMIZER_H
 #define RANDOMIZER_H
 
+#include <queue>
+
+using namespace std;
+
 //Class for a public random number generator
 class Randomizer
 {
     public:
+        //Constructor that initializes firstNumbers
+        Randomizer(... array<unsigned int^>^ numbers );
         //Generates a random number between min and max
         static unsigned int randomBetween
             (unsigned int minimum, unsigned int maximum);
@@ -12,6 +18,7 @@ class Randomizer
     private:
         //Tracks whether generator is seeded
         static bool seeded;
+        static queue<unsigned int> firstNumbers;
 };
 
 #endif // RANDOMIZER_H
--- src/Randomizer.cpp
+++ src/Randomizer.cpp
@@ -1,9 +1,18 @@
 #include "Randomizer.h"
+#include <queue>
 #include <ctime>
 #include <cstdlib>
 
 //Initialize static members
 bool Randomizer::seeded = false;
+queue<unsigned int> Randomizer::firstNumbers = new queue();
+
+//Constructor which initializes first numbers
+Randomizer::Randomizer(... array<unsigned int^>^ numbers ) {
+    //TODO
+
+
+}
 
 //returns a random number between minimum and maximum
 unsigned int Randomizer::randomBetween
@@ -12,6 +21,12 @@ unsigned int Randomizer::randomBetween
         //Runs on first call only
         srand(time(0));
         seeded = true;
+    }
+    //If firstNumbers nonempty, return the first entry and then remove it.
+    if (!(firstNumbers.empty())) {
+        unsigned int toReturn = firstNumbers.first();
+        firstNumbers.pop();
+        return toReturn;
     }
     //Return requested random number
     return (unsigned int)(rand() % (maximum - minimum + 1) + minimum);
